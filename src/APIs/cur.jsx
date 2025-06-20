e3a1d5fcad378306a6c037c8fad46b0bd7823d64
github.com/Ashutosh-programing-2025 <ashutosh.vdoit@gmail.com>
2025-06-20 14:12:49 +0530

    Updated

--- src/APIs/cur.jsx
+++ src/APIs/cur.jsx
@@ -1,19 +1,16 @@
 import axios from 'axios'
 
-const bearerToken = import.meta.env.VITE_TMDB_V4_BEARER;
-
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${bearerToken}`
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzZmMTc4MGQxYzE0YWM1ODM2NmZmMjZjYjNjNjUwZiIsIm5iZiI6MTc1MDA1MjM5Mi41NjA5OTk5LCJzdWIiOiI2ODRmYWUyOGQ3YWU1ZjI5MDcxZWM5N2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-faLJoDYYf1mD6G0lw8wwhtJ-1sVXjsB1aUk-VOGJdU'
   },
 });
 
 export const getApiData = async (value,page=1) => {
   const response = await api.get(`/search/movie?query=${value}&page=${page}`, {
   });
-  console.log(response.data);
   return response.status ===200 ? response.data.results : [];
 };
 
